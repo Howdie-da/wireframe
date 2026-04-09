0348cb1daa681a89c7f7890c10d5d346b5486db0
Akshat Nagar <akshatn9414@gmail.com>
2026-04-09 19:34:03 +0530

    configure redux toolkit store

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router'
 import Layout from './Layout.jsx'
 import { Home } from './components'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth.js'
+import { useEffect, useState } from 'react'
+import { login, logout } from './store/authSlice.js'
 
 
 const router = createBrowserRouter(
@@ -15,9 +19,24 @@ const router = createBrowserRouter(
 )
 
 function App() {
-  return(
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login({userData}))
+      }else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
     <RouterProvider router={router}/>
-  )
+  ) : null
 }
 
 export default App
