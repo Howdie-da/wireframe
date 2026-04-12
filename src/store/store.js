7eb9add0de55ef4526801bf1b4d1b00ed9c1381e
Akshat Nagar <akshatn9414@gmail.com>
2026-04-13 01:08:33 +0530

    fixed the issue of redux to work with non-serializable data

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,17 @@
-import {configureStore} from '@reduxjs/toolkit'
-import authSliceReducer from './authSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import authSliceReducer from './authSlice';
 
 const store = configureStore({
     reducer: {
         auth: authSliceReducer
-    }
-})
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ['auth/login'], 
+                ignoredPaths: ['auth.userData'], 
+            },
+        }),
+});
 
-export default store
+export default store;
